1b74d9dee73a1ce2e0df90e4442fd430af94cd0d
huzheming0302 <418853791@qq.com>
2016-11-29 23:05:19 +0800

    add ProgressBar

--- GoodsShow/src/com/goods/application/GoodsApplication.java
+++ GoodsShow/src/com/goods/application/GoodsApplication.java
@@ -3,6 +3,7 @@ package com.goods.application;
 import com.goods.process.GoodsProcess;
 
 import exhi.net.interface1.INetConfig;
+import exhi.net.log.NetLog;
 import exhi.net.netty.NetApplication;
 import exhi.net.netty.NetProcess;
 
@@ -25,7 +26,7 @@ public class GoodsApplication extends NetApplication {
 		app.setDebugMode(true);
 		GoodsConfig.instance().readConfig();
 		
-		// NetLog.info("Application", GoodsApplication.class.getName());
+		NetLog.info("Application", GoodsApplication.class.getName());
 		
 		app.run(args, "30100D54B46B0CF6CCE7DDEF490C7988");
 	}
